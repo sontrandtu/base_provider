16f2790529d78f4c5764917abc8d87576ef489d0
bxTruong <truongmagic9a@gmail.com>
2022-04-21 16:36:26 +0700

    truong commit 1.0.2

--- lib/application.dart
+++ lib/application.dart
@@ -1,8 +1,10 @@
 import 'package:achitech_weup/common/core/page_manager/page_manager.dart';
 import 'package:achitech_weup/common/core/page_manager/route_path.dart';
 import 'package:achitech_weup/common/core/theme_manager.dart';
+import 'package:achitech_weup/view/theme_view_model.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 
 import 'common/helper/constant.dart';
 
@@ -11,19 +13,20 @@ final GlobalKey<NavigatorState> navigator = GlobalKey<NavigatorState>();
 class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
 
-
   @override
-  Widget build(BuildContext context) =>Consumer<ThemeViewModel>(builder: (context, value, child) =>MaterialApp(
-      navigatorKey: navigator,
-      debugShowCheckedModeBanner: false,
-      title: Constant.app_name,
-      initialRoute: RoutePath.home,
-      darkTheme: ThemeManager.instance.darkTheme,
-      theme: ThemeManager.instance.lightTheme,
-      themeMode: value.mode,
-      onGenerateRoute: generateRoute,
-      locale: context.locale,
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      debugShowMaterialGrid: false) ,) ;
+  Widget build(BuildContext context) => Consumer<ThemeViewModel>(
+        builder: (context, value, child) => MaterialApp(
+            navigatorKey: navigator,
+            debugShowCheckedModeBanner: false,
+            title: Constant.app_name,
+            initialRoute: RoutePath.home,
+            darkTheme: ThemeManager.instance.darkTheme,
+            theme: ThemeManager.instance.lightTheme,
+            themeMode: value.mode,
+            onGenerateRoute: generateRoute,
+            locale: context.locale,
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            debugShowMaterialGrid: false),
+      );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,13 @@ import 'package:achitech_weup/application.dart';
 import 'package:achitech_weup/common/local_storage/app_storage.dart';
 import 'package:achitech_weup/screen/login/login_view_model.dart';
 import 'package:achitech_weup/screen/splash/splash_view_model.dart';
+import 'package:achitech_weup/view/theme_view_model.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+
+import 'common/module/firebase_module.dart';
 
 final SplashViewModel splashViewModel = SplashViewModel();
 final LoginViewModel loginViewModel = LoginViewModel();

4a46172b2ab76cc15bd48598cb84074dffac1b97
tungdm2 <tungdm@weupgroup.vn>
2022-05-06 08:57:04 +0700

    feat: function helper

--- lib/common/extension/integer_extension.dart
+++ lib/common/extension/integer_extension.dart
@@ -69,4 +69,9 @@ extension IntegerExtension on int {
 
     return result;
   }
+
+  bool isPhoneVN() {
+    RegExp regExp = RegExp(r'^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}');
+    return regExp.hasMatch(toString());
+  }
 }
---
+++ lib/common/helper/system_utils.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+
+// ignore: non_constant_identifier_names
+print_r(dynamic input) {
+  if(input != null) {
+    if(input is String || input is num) {
+      debugPrint('$input');
+      return;
+    }
+
+    if(input is Map) {
+      input.forEach((k, v) {
+        debugPrint('${k.runtimeType} - ${v.runtimeType}: $k - $v');
+      });
+      return;
+
+    }
+    if(input is List) {
+      for(var e in input) {
+        debugPrint('${e.runtimeType}: $e');
+      }
+      return;
+    }
+  }
+}

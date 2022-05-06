297ed62dcc650b471c1c53e062286b8b3fa0d6cc
tungdm2 <tungdm@weupgroup.vn>
2022-05-06 18:09:44 +0700

    fix run crop

--- lib/common/helper/image_utils.dart
+++ lib/common/helper/image_utils.dart
@@ -1,22 +1,48 @@
 import 'package:achitecture_weup/common/helper/file_utils.dart';
+import 'package:flutter/material.dart' as material;
 import 'package:image_picker/image_picker.dart';
 import 'dart:io' as io;
+import 'package:image_cropper/image_cropper.dart';
 
 enum PickImageType { image, video }
 
 class ImageUtils {
-
   final ImagePicker _picker = ImagePicker();
+
 
   static Future<dynamic> pickImage({bool hasCrop = false}) async {
     final ImagePicker _p = ImagePicker();
     final XFile? _image = await _p.pickImage(source: ImageSource.gallery);
 
     final rawFile = io.File(_image!.path);
-    bool isSizeMax = FileUtils.imgSize(rawFile);
-
+    bool isSizeMax = await FileUtils.imgSize(rawFile);
     if (!isSizeMax) {
-      if (!hasCrop) {}
+      if (!hasCrop) {
+        return _image.path;
+      } else {
+        CroppedFile? croppedFile = await ImageCropper().cropImage(
+          sourcePath: _image.path,
+          aspectRatioPresets: [
+            CropAspectRatioPreset.square,
+            CropAspectRatioPreset.ratio3x2,
+            CropAspectRatioPreset.original,
+            CropAspectRatioPreset.ratio4x3,
+            CropAspectRatioPreset.ratio16x9
+          ],
+          uiSettings: [
+            AndroidUiSettings(
+                toolbarTitle: 'Cropper',
+                toolbarColor: material.Colors.deepOrange,
+                toolbarWidgetColor: material.Colors.white,
+                initAspectRatio: CropAspectRatioPreset.original,
+                lockAspectRatio: false),
+            IOSUiSettings(
+              title: 'Cropper',
+            ),
+          ],
+        );
+        return croppedFile!.path;
+      }
     } else {
       return null;
     }
@@ -31,4 +57,10 @@ class ImageUtils {
       return _video!.path;
     }
   }
+}
+
+class _ImageCrop {
+  _ImageCrop._();
+
+
 }

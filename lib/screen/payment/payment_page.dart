db41e0e6029de0c1023e1cf71ad75277514942d4
bxTruong <truongbx@weupgroup.vn>
2022-06-07 09:54:56 +0700

    truong commit scale_ani_button_comp.dart va position_ani_button_comp.dart

--- lib/screen/payment/payment_page.dart
+++ lib/screen/payment/payment_page.dart
@@ -37,7 +37,7 @@ class _PaymentLayout extends StatelessWidget {
       builder: (context, model, child) {
         return ListView(
           children: [
-            const ImageViewer(
+             const ImageViewerComp(
               'https://hoithanh.com/wp-content/uploads/2015/07/b7433357-de29-4381-9cd4-9c2b8882f4c0.jpg',
               // 'assets/images/pdf.png',
               hasViewImage: true,

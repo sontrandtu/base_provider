5219fbe3319662ce903a3cac4067d9ac8342f23a
Đỗ Mạnh Tài <manhtai831@gmail.com>
2022-09-07 10:53:40 +0700

    update phụ thuộc giữa các class lỏng hơn

---
+++ dependencies/widgets/lib/widgets.dart
@@ -0,0 +1,55 @@
+
+/// CODE GENERATE
+
+library widgets;
+
+export '/src/ani_appbar_title_comp.dart';
+export '/src/appbar_comp.dart';
+export '/src/avatar_comp.dart';
+export '/src/base_responsive.dart';
+export '/src/dropdown_comp.dart';
+export '/src/error_layout.dart';
+export '/src/form_album.dart';
+export '/src/form_number.dart';
+export '/src/html_content.dart';
+export '/src/indicator_comp.dart';
+export '/src/ink_well_comp.dart';
+export '/src/mapviewer_comp.dart';
+export '/src/multiple_select.dart';
+export '/src/no_connect_layout.dart';
+export '/src/pdf_viewer_comp.dart';
+export '/src/rating_comp.dart';
+export '/src/shimmer_comp.dart';
+export '/src/undefined_layout.dart';
+export '/src/bottombar/bottom_bar_fab_comp.dart';
+export '/src/bottombar/painter_bottombar.dart';
+export '/src/bottom_sheet/cupertino_picker.dart';
+export '/src/bottom_sheet/multiple_selec_bottomsheet.dart';
+export '/src/bottom_sheet/pick_img_bottomsheet.dart';
+export '/src/button/cupertino_swtich_comp.dart';
+export '/src/button/elevated_button_comp.dart';
+export '/src/button/floating_action_button_comp.dart';
+export '/src/button/icon_button_comp.dart';
+export '/src/button/outlined_button_comp.dart';
+export '/src/button/position_ani_button_comp.dart';
+export '/src/button/scale_ani_button_comp.dart';
+export '/src/button/switch_comp.dart';
+export '/src/button/text_button_comp.dart';
+export '/src/check_box/check_box_comp.dart';
+export '/src/check_box/check_box_custom_comp.dart';
+export '/src/dialog/custom_dialog.dart';
+export '/src/dialog/date_picker_custom_dialog.dart';
+export '/src/image/cached_network_image_comp.dart';
+export '/src/image/image_viewer_comp.dart';
+export '/src/image/slider_comp.dart';
+export '/src/image/svg_comp.dart';
+export '/src/image/view_image.dart';
+export '/src/input/button_obscure.dart';
+export '/src/input/outline_input.dart';
+export '/src/input/underline_input.dart';
+export '/src/model_base/option_dropdown.dart';
+export '/src/model_base/option_multiple_select.dart';
+export '/src/radio/radio_comp.dart';
+export '/src/radio/radio_custom_comp.dart';
+export '/src/refresh/base_refresh_controller.dart';
+export '/src/refresh/smart_refresh_comp.dart';
